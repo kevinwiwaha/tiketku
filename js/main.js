f5aa1accc112ab8dd9476fd03103ab2a036e5c80
kevinwiwaha <kevinwiwaha@gmail.com>
2020-06-23 09:17:41 +0700

    menambahkan event carousel + deskripsi

--- js/main.js
+++ js/main.js
@@ -1,8 +1,21 @@
 $(document).ready(function () {
-    $(".owl-carousel").owlCarousel({
-        items: 4,
-        lazyLoad: true,
+    $('.owl-carousel').owlCarousel({
         loop: true,
-        margin: 10
-    });
+
+        responsiveClass: true,
+        responsive: {
+            0: {
+
+                margin: 50,
+                loop: true
+            },
+
+            1000: {
+                items: 3.8,
+                margin: 50,
+                loop: true,
+
+            }
+        }
+    })
 });
